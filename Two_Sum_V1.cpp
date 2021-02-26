217e457d706f739b3215398d5cb4ce020164135b
Fernando Hernandez <ffhf.1820@gmail.com>
2021-02-25 22:56:05 -0300

    Update Two_Sum_V1.cpp

--- Two_Sum_V1.cpp
+++ Two_Sum_V1.cpp
@@ -1,20 +1,21 @@
+// Brute Force
+
 class Solution {
-public:
+  public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int index = 0;
         vector<int> pairResult;
-        
-         for (int i = index; i < nums.size() - 1; i++)
+
+         for (int i = 0; i < nums.size() -1 ; i++)
          {
-            for (int j = index + 1; j < nums.size(); j++)
+            for (int j = i + 1; j < nums.size(); j++)
             {
-                if (nums[i] + nums[j] == target)
+                if (nums[j] + nums[i] == target)
                 {
                     pairResult.push_back(i);
                     pairResult.push_back(j);
                 }
             }  
          }
-        return pairResult;
-    };
+    return pairResult;
+   };
 };
