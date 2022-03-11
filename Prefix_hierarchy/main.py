77c87abf84af7e16893dcd758694d5fb72c37454
FernandoFH <ffhf.1820@gmail.com>
2022-03-11 14:00:13 -0300

    Uddate: Prefix finish

--- Prefix_hierarchy/main.py
+++ Prefix_hierarchy/main.py
@@ -5,4 +5,23 @@
 # Prefix ['Mart', 'Mar', 'Mong']
 # Output: [2, 4, 1]
 
-print("Hi!")
+
+def prefix_hierarchy(input_list, query):
+    """
+    input_list: list of strings
+    query: list of strings
+    output: list of int
+    """
+    output = []
+    counter = 0
+
+    for j in range(len(query)):
+        for i in range(len(input_list)):
+            if (input_list[i].startswith(query[j]) and  (len(input_list[i]) != len(query[j]))):
+                counter += 1
+        output.append(counter)
+        counter = 0
+        
+    return output
+
+print(prefix_hierarchy(['Martin', 'Pedro', 'Maria', 'Martina', 'Marina', 'Mongo'], ['Mart', 'Mar', 'Mong']))
